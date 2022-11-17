121e9841dad0ef1b5921287d801cbb202556f31f
ghstN <98927555+ghstN@users.noreply.github.com>
2022-11-16 22:48:17 -0300

    Add files via upload

---
+++ trabalho barreto n3/trabalho.js
@@ -0,0 +1,15 @@
+//inteiro aleatorio entre 1 e 10 
+//usuario é solicitado pedir um numero de adivinhação 
+//se entrada for igual palpite, exibirá bom trabalho, se nao é erro
+
+// let numeroUsuario = input("escolha entre 1 - 10")
+
+
+// if (numeroUsuario=5){
+//     alert("Bom trabalho")
+// }
+// else{
+//     alert("tente de novo", numeroUsuario)
+    
+
+// }
